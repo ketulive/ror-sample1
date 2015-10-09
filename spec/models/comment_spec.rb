45814053473914a249e9af85464b1ebaac0c0217
Yousuke Nii <yousuke.nii@gmail.com>
2015-10-10 01:03:10 +0900

    Add create comment api

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -34,3 +34,14 @@ RSpec.describe Comment, type: :model do
     end
   end
 end
+
+RSpec.describe Comment, type: :request do
+  let(:image) { Image.create(url: "http://example.com/", title: "test") }
+
+  describe "POST /images/:id/comments.json" do
+    it "create comments" do
+      post "/images/#{image.id}/comments.json", comment: { body: "test" }
+      expect(response.status).to be 201
+    end
+  end
+end
